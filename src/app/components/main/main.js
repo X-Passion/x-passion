2c80c8b8f918f8c41db7cc5882d3fd95f71bbf93
tizot <masset.camille@gmail.com>
2015-06-29 17:10:26 +0200

    AngularJS app structure

---
+++ src/app/components/main/main.js
@@ -0,0 +1,189 @@
+'use strict';
+
+angular.module('xpassion.main', [
+    //
+])
+
+.config(['$stateProvider', '$urlMatcherFactoryProvider', 
+    function($stateProvider, $urlMatcherFactoryProvider) {
+    //$urlMatcherFactoryProvider.strictMode(false);
+    $stateProvider
+        .state('index', {
+            url: "/",
+            views: {
+                '@': {
+                    templateUrl: "app/components/main/layout.html",
+                    controller: 'main.ctrl.base'
+                },
+                'header@index': {
+                    templateUrl: "app/components/main/header.html",
+                    controller: 'main.ctrl.header'
+                },
+                '@index': {
+                    templateUrl: "app/components/main/home.html",
+                    controller: 'main.ctrl.home'
+                }
+            }
+        })
+        // .state('index.presentation', {
+        //     url: "presentation",
+        //     templateUrl: "components/static/presentation.html",
+        //     controller: ['$scope', function($scope) {
+        //         $scope.infos.active = 'presentation';
+        //     }]
+        // })
+        
+        // .state('index.login', {
+        //     url: "login",
+        //     templateUrl: "components/common/login.html",
+        //     controller: 'main.ctrl.login'
+        // })
+        // .state('index.forgot-password', {
+        //     url: "forgot-password",
+        //     templateUrl: "components/common/forgot-password.html",
+        //     controller: 'main.ctrl.forgot_pwd'
+        // })
+        // .state('index.external-links', {
+        //     url: "liens",
+        //     templateUrl: "components/static/liens.html",
+        //     controller: ['$scope', function($scope){
+        //         $scope.infos.active = '';
+        //     }]
+        // })
+        // .state('index.infos-pratiques', {
+        //     url: "infos-pratiques",
+        //     templateUrl: "components/static/infos-pratiques.html",
+        //     controller: ['$scope', function($scope){
+        //         $scope.infos.active = '';
+        //     }]
+        // })
+        // .state('index.gallery', {
+        //     url: "gallery",
+        //     templateUrl: "components/static/gallery.html",
+        //     controller: ['$scope', 'Gallery', function($scope, Gallery) {
+        //         $scope.infos.active = 'gallery';
+        //         $scope.images = Gallery.query();
+        //         $scope.openGallery = function(i) {
+        //             var gallery = blueimp.Gallery($scope.images, { index: i });
+        //         };
+        //     }]
+        // })
+        .state('index.404', {
+            url: "404",
+            templateUrl: "components/static/404.html",
+            controller: ['$scope', '$timeout', '$state', function($scope, $timeout, $state) {
+                $scope.infos.active = '';
+                // $timeout(function() {
+                //     $state.go('index.presentation')
+                // }, 3500);
+            }]
+        })
+    ;
+}])
+
+.controller('main.ctrl.base',
+    ['$scope', '$stateParams', /*'AuthService',*/ '$state', '$modal', 
+    function($scope, $stateParams, /*AuthService,*/ $state, $modal) {
+        $scope.infos = {
+            active: 'index',
+            // user: AuthService.getUser()
+        };
+        // $scope.isLoggedIn = function() {
+        //     return AuthService.isAuthenticated();
+        // };
+        // $scope.logout = function() {
+        //     $scope.infos.user = null;
+        //     AuthService.logout();
+        //     $state.go('index');
+        // };
+
+
+        // $scope.loginOpen = function(size) {
+        //     var modalInstance = $modal.open({
+        //         templateUrl: "components/common/login.html",
+        //         controller: 'main.ctrl.login',
+        //         size: size
+        //     });
+
+        //     modalInstance.result.then(function (user) {
+        //         $scope.infos.user = user;
+        //     });
+        // };
+    }]
+)
+
+.controller(
+    'main.ctrl.header',
+    ['$scope', /*'AuthService', */
+    function($scope/*, AuthService*/) {
+        //
+    }]
+)
+
+.controller('main.ctrl.home', 
+    ['$scope', '$state',
+    function($scope, $state) {
+        $scope.infos.active = '';
+    }]
+)
+
+// .controller('main.ctrl.login', 
+//     ['$scope', 'AuthService', '$modalInstance', '$state', '$timeout', 
+//     function($scope, AuthService, $modalInstance, $state, $timeout){
+//         $scope.login = {
+//             username: '',
+//             password: ''
+//         };
+
+//         $timeout(function () {
+//             document.getElementById("usernameInput").focus();
+//         }, 300);
+
+//         $scope.alerts = [];
+
+//         $scope.closeAlert = function(index) {
+//             $scope.alerts.splice(index, 1);
+//         };
+
+//         $scope.connexion = function (login) {
+//             $scope.loginError = false;
+//             $scope.inLogin = true;
+//             AuthService.login(login).then(
+//                 function(user) {
+//                     $scope.login = {username: '', password: ''};
+//                     //$scope.infos.user = AuthService.getUser();
+//                     $scope.inLogin = false;
+//                     $modalInstance.close(user);
+//                 }, function(error) {
+//                     $scope.loginError = true;
+//                     $scope.alerts.push({type: 'danger', msg: 'Echec de l\'authentification.'});
+//                     $scope.login.password = '';
+//                     $scope.inLogin = false;
+//                 }
+//             );
+//         };
+
+//         $scope.cancel = function(dest) {
+//             $modalInstance.dismiss('cancel');
+//             if (dest == 'forgot') {
+//                 $state.go('index.forgot-password');
+//             }
+//         }
+//     }]
+// )
+
+// .controller('main.ctrl.forgot_pwd',
+//     ['$scope', 'User', 
+//     function($scope, User) {
+//         $scope.infos.active = '';
+
+//         $scope.generatePwd = function() {
+//             User.forgot_password({email: $scope.uemail}).$promise.then(function(res) {
+//                 console.log(res);
+//             }, function(error) {
+//                 console.log(error);
+//             });
+//         };
+//     }]
+// )
+;
--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,128 @@
 'use strict';
 
-angular.module('xpassion', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap'])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
+angular.module('xpassion', [
+    'angularMoment',
+    'ngAnimate', 
+    'ngCookies', 
+    'ngTouch', 
+    'ngSanitize', 
+    'ngResource', 
+    'ui.router', 
+    'ui.bootstrap',
+    'angularFileUpload',
 
-    $urlRouterProvider.otherwise('/');
-  })
+    'xpassion.main'
+])
+
+// .factory('http.request.interceptor', 
+//     ['$q', '$location',
+//     function ($q, $location) {
+//         return {
+//             'responseError': function(rejection) {
+//                 // do something on error
+//                 if(rejection.status === 404){
+//                     $location.path('/404');                    
+//                 }
+//                 return $q.reject(rejection);
+//              }
+//          };
+//     }]
+// )
+
+.config(['$urlRouterProvider',
+    function($urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+    }]
+)
+
+// .config(['$httpProvider',
+//     function ($httpProvider) {
+//         $httpProvider.interceptors.push('auth.interceptor');
+//         $httpProvider.interceptors.push('http.request.interceptor');
+//     }]
+// )
+
+.directive('ngThumb', ['$window', function($window) {
+    var helper = {
+        support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+        isFile: function(item) {
+            return angular.isObject(item) && item instanceof $window.File;
+        },
+        isImage: function(file) {
+            var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+            return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+        }
+    };
+
+    return {
+        restrict: 'A',
+        template: '<canvas/>',
+        link: function(scope, element, attributes) {
+            if (!helper.support) return;
+
+            var params = scope.$eval(attributes.ngThumb);
+
+            if (!helper.isFile(params.file)) return;
+            if (!helper.isImage(params.file)) return;
+
+            var canvas = element.find('canvas');
+            var reader = new FileReader();
+
+            reader.onload = onLoadFile;
+            reader.readAsDataURL(params.file);
+
+            function onLoadFile(event) {
+                var img = new Image();
+                img.onload = onLoadImage;
+                img.src = event.target.result;
+            }
+
+            function onLoadImage() {
+                var width = params.width || this.width / this.height * params.height;
+                var height = params.height || this.height / this.width * params.width;
+                canvas.attr({ width: width, height: height });
+                canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+            }
+        }
+    };
+}])
+
+.filter('civility', function() {
+    return function(text) {
+        if (text == 'Monsieur') {
+            return 'M.';
+        } else if (text == 'Madame') {
+            return 'Mme';
+        } else if (text == 'Mademoiselle') {
+            return 'Mlle';
+        } else {
+            return '';
+        }
+    };
+})
+.filter('nl2br', function(){
+      return function(text) {
+           return text ? text.replace(/\n/g, '<br/>') : '';
+      };
+})
+
+.run(['amMoment', '$rootScope',
+    function(amMoment, $rootScope) {
+        moment.locale('fr', {
+            calendar : {
+                lastDay : '[Hier]',
+                sameDay : "[Aujourd'hui]",
+                nextDay : '[Demain]',
+                lastWeek : 'dddd [dernier]',
+                nextWeek : 'dddd [prochain]',
+                sameElse : 'L'
+            }
+        });
+        amMoment.changeLocale('fr');
+    }
+])
+
+.constant('angularMomentConfig', {
+    timezone: 'Europe/Paris' // e.g. 'Europe/London'
+})
 ;
