a17db8f8b289ffffa4b77128f3393000b3a11203
tizot <masset.camille@gmail.com>
2015-08-08 13:52:28 +0200

    Adapt login to new django API

--- src/app/components/auth/auth.js
+++ src/app/components/auth/auth.js
@@ -15,7 +15,7 @@ angular.module('xpassion.auth', [
         }
         return {
             login: function(credentials, resultLogin) {
-                return $injector.get('$http').post(API.route('users/login'), credentials).then(
+                return $injector.get('$http').post(API.route('api-token-auth/'), credentials).then(
                     function(response) {
                         $localStorage.auth.token = response.data.token;
                         $localStorage.auth.user = response.data.user;
@@ -25,7 +25,6 @@ angular.module('xpassion.auth', [
                     function(response) {
                         $localStorage.auth.token = null;
                         $localStorage.auth.user = null;
-                        //$location.path('/login');
                         return $q.reject();
                     });
             },
@@ -49,21 +48,14 @@ angular.module('xpassion.auth', [
     function (AuthService, $q, $location) {
         return {
             request: function(config) {
-                // config.headers = config.headers || {};
-                // if (AuthService.isAuthenticated()) {
-                //  config.headers.Authorization = 'Bearer ' + AuthService.getToken();
-                // }
-
                 config.params = config.params || {};
-                // to improve: necessary for ui.bootstrap ; and the token is useless for static files
-                if (AuthService.isAuthenticated() && /api\//.test(config.url)) {
-                    config.headers["X-Token"] = AuthService.getToken();
+                if (AuthService.isAuthenticated()) {
+                    config.headers["Authorization"] = "JWT " + AuthService.getToken();
                 }
                 return config || $q.when(config);
             },
             response: function(response) {
                 if (response.status === 401) {
-                    //AuthService.logout();
                     $state.go('index.login');
                 }
                 return response || $q.when(response);
--- src/app/components/main/api.js
+++ src/app/components/main/api.js
@@ -9,26 +9,14 @@ angular.module('xpassion.api', [
         return { 
             route: function (path) {
                 if (/localhost/.test($location.absUrl())) {
-                    return 'http://localhost/~camille/x-passion-api/index.php' + (path == '' ? '' : '/' + path);
+                    return 'http://127.0.0.1:8000' + (path == '' ? '' : '/' + path);
                 } else {
-                    return 'api/index.php' + (path == '' ? '' : '/' + path);
+                    return 'api' + (path == '' ? '' : '/' + path);
                 }
             }
         };
     }
 ])
-
-.factory('User', ['API', '$resource', 
-    function(API, $resource) {
-        return $resource(API.route('users/:id'), {id: '@id'}, {
-            enable: {method: 'PUT', url: API.route('users/enable/:id')},
-            disable: {method: 'PUT', url: API.route('users/disable/:id')},
-            change_password: {method: 'PUT', url: API.route('users/change-password')},
-            forgot_password: {method: 'PUT', url: API.route('users/forgot-password')},
-            update: {method: 'PUT', url: API.route('users/:id')}
-        }, {stripTrailingSlashes: false});
-    }]
-)
 
 .factory('Comment', ['API', '$resource',
     function(API, $resource) {
