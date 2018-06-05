f608d5347233b4932e6b51191fd9964652d83404
Étienne Morice <pro.emorice@mail.com>
2018-06-05 17:41:20 +0200

    skipping depencies trick

--- src/app/components/survey/survey.js
+++ src/app/components/survey/survey.js
@@ -13,23 +13,39 @@ angular.module('xpassion.survey', [])
 			url: 'sondage',
 			templateUrl: 'app/components/survey/intro.html',
 			controller: 'survey.ctrl.intro',
-		});
+		})
+		.state('index.survey.body', {
+			url: 'sondage/repondre',
+			template: '<xpassion:survey />',
+			controller: ['$scope', function($scope) {
+			}]
+		})
+	;
 }])
 
-.controller('survey.ctrl.intro', ['$scope', '$document', 'AuthService', 'API', 'Survey',
-	function($scope, $document, AuthService, API, Survey) {
-		$scope.infos.active = 'survey';
-		$scope.frankizUrl = 'https://cas.binets.fr/login?service=' + encodeURIComponent(
-			API.route('api-brcas-token-auth/?next=' + encodeURIComponent(
-				$document[0].URL
-			)));
-		$scope.surveyStatus = 0;
-		AuthService.ready().then(function() {
-			$scope.survey = Survey.get({}, function(value, responseHeaders) {
-				$scope.surveyStatus = 200;
-			}, function(response) {
-				$scope.surveyStatus = response.status;
-			});
+.controller('survey.ctrl.intro', ['$scope', '$document', 'AuthService', 'API', 'Survey', function($scope, $document, AuthService, API, Survey) {
+	$scope.infos.active = 'survey';
+	$scope.frankizUrl = 'https://cas.binets.fr/login?service=' + encodeURIComponent(
+		API.route('api-brcas-token-auth/?next=' + encodeURIComponent(
+			$document[0].URL
+		)));
+	$scope.surveyStatus = 0;
+	AuthService.ready().then(function() {
+		$scope.survey = Survey.get({}, function(value, responseHeaders) {
+			$scope.surveyStatus = 200;
+		}, function(response) {
+			$scope.surveyStatus = response.status;
 		});
-	}])
+	});
+}])
+
+.controller('survey.ctrl.body', ['$scope', function($scope) {
+}])
+
+.directive('xpassionSurvey', ['', function() {
+	return {
+		controller: 'survey.ctrl.body',
+		templateUrl: 'app/components/survey/body.html',
+	};
+}])
 ;
